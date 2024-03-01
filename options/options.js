5a4d872539498d6155e775f54cc217a77dda84a8
Crowdbotics <team@crowdbotics.com>
2024-03-01 03:28:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-waterfall-46740",
+  url: "https://proud-waterfall-46740.botics.co",
+  api: "https://proud-waterfall-46740.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
